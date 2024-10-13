66c0c97356fee260d9df23ca6570e694f2912312
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-10-13 16:31:56 +0200

    use Path and Files static methods instead, can writeString

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -2,16 +2,67 @@ package dev.lpa;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Main {
 
   public static void main(String[] args) {
 
-  userFile("testFile.txt");
+    useFile("testFile.txt");  // File Class IO package
+
+    usePath("pathFile.txt");
 
   }
 
-  private static void userFile(String fileName) {
+  private static void usePath(String fileName) {
+
+    Path path = Path.of(fileName);  // JDK 11
+    boolean fileExists = Files.exists(path);
+
+    System.out.printf("File '%s' %s%n", fileName, fileExists ? "exists." : "doesn't exist.");
+
+    if (fileExists) {
+      System.out.println("Deleting File: " + fileName);
+      try {
+        Files.delete(path);
+        fileExists = false;
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+
+    try {
+      if (!fileExists) {
+        Files.createFile(path);
+        System.out.println("Created File: " + fileName);
+        if (Files.isWritable(path)) {  // File method has no way to write by itself
+//          System.out.println("Would write to file here.");
+          Files.writeString(path, """
+            Here is some data,
+            For my file,
+            just to prove,
+            Using the Files class and path are better!
+            """);
+        } else {
+          System.out.println("File is not writable.");
+        }
+        if (Files.isReadable(path)) {
+          System.out.println("----------------------------------");
+          System.out.println("Reading File: ");
+          Files.readAllLines(path).forEach(System.out::println);
+        } else {
+          System.out.println("File is not readable.");
+        }
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+
+  }
+
+  private static void useFile(String fileName) {
 
     File file = new File(fileName);
     boolean fileExists = file.exists();
@@ -32,6 +83,8 @@ public class Main {
       System.out.println("Created File: " + fileName);
       if (file.canWrite()) {  // File method has no way to write by itself
         System.out.println("Would write to file here.");
+      } else {
+        System.out.println("File is not writable.");
       }
     }
 
