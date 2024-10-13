5195452a965a0a9772ba52065e1e6ceeb2d04306
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-10-13 15:43:27 +0200

    show with File exists delete create (IO package)

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,9 +1,39 @@
 package dev.lpa;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
 
   public static void main(String[] args) {
 
+  userFile("testFile.txt");
+
+  }
+
+  private static void userFile(String fileName) {
+
+    File file = new File(fileName);
+    boolean fileExists = file.exists();
+
+    System.out.printf("File '%s' %s%n", fileName, fileExists ? "exists." : "doesn't exist.");
+
+    if (fileExists) {
+      System.out.println("Deleting File: " + fileName);
+      fileExists = !file.delete();
+    }
+
+    if (!fileExists) {
+      try {
+        file.createNewFile();
+      } catch (IOException e) {
+        System.out.println("Something went wrong.");
+      }
+      System.out.println("Created File: " + fileName);
+      if (file.canWrite()) {  // File method has no way to write by itself
+        System.out.println("Would write to file here.");
+      }
+    }
 
   }
 }
